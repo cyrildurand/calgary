dcaca1a6b2eee49416fb86759bef458c01743b6c
Cyril DURAND <contact@cyrildurand.net>
2018-11-20 14:04:57 +0100

    dotenv settings for contentfull access key

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,39 @@
+require("dotenv").config();
+
+let contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  accessToken: process.env.CONTENTFUL_ENVIRONMENT | "master"
+};
+
 module.exports = {
-    siteMetadata: {
-        siteUrl : 'http://localhost:8000'
+  siteMetadata: {
+    siteUrl: "http://localhost:8000"
+  },
+  plugins: [
+    "gatsby-plugin-typescript",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+        environment: process.env.NODE_ENV
+      }
     },
-    plugins: [
-        'gatsby-plugin-typescript',
-        {
-            resolve: `gatsby-source-contentful`,
-            options: {
-                spaceId: `at9a6gmz24f5`,
-                accessToken: `f16f4802d47cbccd31f69d4eda867cda19636b8562412f623cf5d0da8ab52a8c`
-            }
-        },
-        'gatsby-plugin-sitemap',
-        'gatsby-plugin-offline',
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: "GatsbyJS",
-                short_name: "GatsbyJS",
-                start_url: "/",
-                background_color: "#6b37bf",
-                theme_color: "#6b37bf",
-                display: "minimal-ui",
-                icon: "src/images/favicon-512.png", // This path is relative to the root of the site.
-            },
-        },
-        `gatsby-plugin-react-helmet`
-    ]
-}
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "minimal-ui",
+        icon: "src/images/favicon-512.png" // This path is relative to the root of the site.
+      }
+    },
+    `gatsby-plugin-react-helmet`
+  ]
+};
