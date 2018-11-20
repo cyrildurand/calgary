874bf53ac94ff4fa5693fab04384f32c30cec34e
Cyril DURAND <contact@cyrildurand.net>
2018-11-20 14:29:19 +0100

    fix config error in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,41 @@
-require("dotenv").config();
+require('dotenv').config();
 
 let contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  accessToken: process.env.CONTENTFUL_ENVIRONMENT | "master"
+  environment: process.env.CONTENTFUL_ENVIRONMENT || 'master'
 };
+
+console.log(contentfulConfig);
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "http://localhost:8000"
+    siteUrl: 'http://localhost:8000'
   },
   plugins: [
-    "gatsby-plugin-typescript",
+    'gatsby-plugin-typescript',
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: process.env.SPACE_ID,
-        accessToken: process.env.ACCESS_TOKEN,
-        environment: process.env.NODE_ENV
+        spaceId: contentfulConfig.spaceId,
+        accessToken: contentfulConfig.accessToken,
+        environment: contentfulConfig.environment
       }
     },
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        display: "minimal-ui",
-        icon: "src/images/favicon-512.png" // This path is relative to the root of the site.
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon-512.png' // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-react-helmet`
+    'gatsby-plugin-react-helmet'
   ]
 };
