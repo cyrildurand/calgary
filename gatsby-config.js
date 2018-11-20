ce18a3453f35415a96f78027f32e2a99e3548150
Cyril DURAND <contact@cyrildurand.net>
2018-11-20 01:02:17 +0100

    fix sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    siteMetadata: {
+        siteUrl : 'http://localhost:8000'
+    },
     plugins: [
         'gatsby-plugin-typescript',
         {
@@ -8,20 +11,20 @@ module.exports = {
                 accessToken: `f16f4802d47cbccd31f69d4eda867cda19636b8562412f623cf5d0da8ab52a8c`
             }
         },
-        // 'gatsby-plugin-sitemap',
+        'gatsby-plugin-sitemap',
         'gatsby-plugin-offline',
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-              name: "GatsbyJS",
-              short_name: "GatsbyJS",
-              start_url: "/",
-              background_color: "#6b37bf",
-              theme_color: "#6b37bf",
-              display: "minimal-ui",
-              icon: "src/images/favicon-512.png", // This path is relative to the root of the site.
+                name: "GatsbyJS",
+                short_name: "GatsbyJS",
+                start_url: "/",
+                background_color: "#6b37bf",
+                theme_color: "#6b37bf",
+                display: "minimal-ui",
+                icon: "src/images/favicon-512.png", // This path is relative to the root of the site.
             },
-          },
-          `gatsby-plugin-react-helmet`
+        },
+        `gatsby-plugin-react-helmet`
     ]
 }
